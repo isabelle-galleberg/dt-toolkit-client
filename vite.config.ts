3b4de91b7facb061a9744af7409d00ae4ff2072f
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-18 10:47:31 +0100

    fix: add base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/dt-toolkit-client/', // Required for GitHub Pages
   plugins: [react()],
   define: {
     'process.env': process.env,
