cf89980788704641684c46b55483b26da3d114d2
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-20 13:17:27 +0100

    fix: route back to previous page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useUserStore } from '../store/userStore';
 
 function NotFoundPage() {
+  const navigate = useNavigate();
+  const { user } = useUserStore();
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
-        <div className="max-w-md">
+        <div>
           <h1 className="text-3xl font-bold">404 - Lost in Brainstorming 💭</h1>
           <div className="py-6">
             <p> Oops, you went a bit too far outside the box!</p>
             <p> Let’s ideate our way back to something that actually exists.</p>
           </div>
-          <Link to="/">
-            <button className="btn btn-primary"> Back to main page 🚀</button>
-          </Link>
+          <button
+            onClick={() => navigate(user?.page || '/design-thinking')}
+            className="btn btn-primary"
+          >
+            Back to civilization 🚀
+          </button>
         </div>
       </div>
     </div>
