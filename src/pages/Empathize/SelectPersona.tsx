8f1717a8547f6ff9c6f08d4523649648a2a4a2e8
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-20 14:15:16 +0100

    feat: loading spinner component

--- src/pages/Empathize/SelectPersona.tsx
+++ src/pages/Empathize/SelectPersona.tsx
@@ -4,6 +4,8 @@ import { usePersonaStore } from '../../store/personaStore';
 import { useState, useEffect, useRef } from 'react';
 import { PersonaCard } from '../../types/persona';
 import { getPersonaCards } from '../../services/personaCardService';
+import LoadingSpinner from '../../components/LoadingSpinner';
+
 function SelectPersona() {
   const { persona, setPersona } = usePersonaStore();
   const [personaCards, setPersonaCards] = useState<PersonaCard[]>([]);
@@ -56,66 +58,63 @@ function SelectPersona() {
   };
 
   return (
-    <div className="flex flex-col items-center px-4">
-      <ActivityPageLayout
-        header="Who will you play as?"
-        phase="Empathize"
-        phaseColor="text-empathize"
-        text={
-          <>
-            Phishers don't trick everyone the same way!
-            <br />
-            Pick a character and see how they deal with online scams:
-          </>
-        }
-        activity={
-          <div className="w-[780px]">
-            {loading ? (
-              <div className="flex items-center justify-center h-[calc(100vh-45rem)]">
-                <span className="loading loading-ring w-32 h-32 text-empathize"></span>
-              </div>
-            ) : (
-              <div className="flex items-center gap-4">
-                <div className="h-[300px] flex items-center">
-                  <button
-                    onClick={handlePrevCard}
-                    className="p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4"
-                  >
-                    &lt;
-                  </button>
-                </div>
-                <div
-                  ref={cardContainerRef}
-                  className="flex gap-4 overflow-x-auto scroll-smooth w-full"
-                  style={{
-                    scrollBehavior: 'smooth',
-                  }}
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <ActivityPageLayout
+          header="Who will you play as?"
+          phase="Empathize"
+          phaseColor="text-empathize"
+          text={
+            <>
+              Phishers don't trick everyone the same way!
+              <br />
+              Pick a character and see how they deal with online scams:
+            </>
+          }
+          activity={
+            <div className="flex items-center gap-4 w-[780px]">
+              <div className="h-[300px] flex items-center">
+                <button
+                  onClick={handlePrevCard}
+                  className="p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4"
                 >
-                  {personaCards.map((card) => (
-                    <img
-                      key={card._id}
-                      src={card.cardImageUrl}
-                      alt="persona-card"
-                      className={`w-52 cursor-pointer rounded-lg transition-all duration-300 
-                ${persona?._id === card._id ? 'scale-110 shadow-lg' : ''}`}
-                      onClick={() => handlePersonaSelection(card)}
-                    />
-                  ))}
-                </div>
-                <div className="h-[300px] flex items-center">
-                  <button
-                    onClick={handleNextCard}
-                    className="p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4"
-                  >
-                    &gt;
-                  </button>
-                </div>
+                  &lt;
+                </button>
               </div>
-            )}
-          </div>
-        }
-      />
-    </div>
+              <div
+                ref={cardContainerRef}
+                className="flex gap-4 overflow-x-auto scroll-smooth w-full"
+                style={{
+                  scrollBehavior: 'smooth',
+                }}
+              >
+                {personaCards.map((card) => (
+                  <img
+                    key={card._id}
+                    src={card.cardImageUrl}
+                    alt="persona-card"
+                    className={`w-52 cursor-pointer rounded-lg transition-all duration-300 
+                ${persona?._id === card._id ? 'scale-110 shadow-lg' : ''}`}
+                    onClick={() => handlePersonaSelection(card)}
+                  />
+                ))}
+              </div>
+              <div className="h-[300px] flex items-center">
+                <button
+                  onClick={handleNextCard}
+                  className="p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4"
+                >
+                  &gt;
+                </button>
+              </div>
+            </div>
+          }
+        />
+      )}
+    </>
   );
 }
+
 export default SelectPersona;
