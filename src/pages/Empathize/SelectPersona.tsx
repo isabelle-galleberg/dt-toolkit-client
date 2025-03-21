bc3febd6eca3495545513ff06e65e375b0e271a4
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-21 13:17:03 +0100

    fix: return loading state

--- src/pages/Empathize/SelectPersona.tsx
+++ src/pages/Empathize/SelectPersona.tsx
@@ -9,7 +9,7 @@ import LoadingSpinner from '../../components/LoadingSpinner';
 import { useUserStore } from '../../store/userStore';
 
 // shuffle array based on a seed
-const seededShuffle = (array: PersonaCard[], seed: string) => {
+const seededShuffle = (array: PersonaCard[], seed: string): PersonaCard[] => {
   const rng = seedrandom(seed);
   return array
     .map((item) => ({ item, sort: rng() }))
@@ -25,28 +25,23 @@ function SelectPersona() {
   const [loading, setLoading] = useState<boolean>(true);
   const { user } = useUserStore();
   const userId = user?._id || '';
-
   const [isAtStart, setIsAtStart] = useState(true);
   const [isAtEnd, setIsAtEnd] = useState(false);
 
   useEffect(() => {
-    if (persona && !isTaskComplete('/empathize/select-persona')) {
-      markTaskComplete('/empathize/select-persona');
-    }
-  }, [persona]);
-
-  useEffect(() => {
-    setLoading(true);
-    (async () => {
+    const fetchPersonaCards = async () => {
+      setLoading(true);
       try {
         const cards = await getPersonaCards();
-        const shuffledCards = seededShuffle(cards, userId); // shuffle persona cards based on user ID
+        const shuffledCards = seededShuffle(cards, userId); // shuffle based on user ID
         setPersonaCards(shuffledCards);
-        setLoading(false);
       } catch (error) {
         console.error('Error fetching persona cards:', error);
+      } finally {
+        setLoading(false);
       }
-    })();
+    };
+    fetchPersonaCards();
   }, [userId]);
 
   useEffect(() => {
@@ -79,6 +74,12 @@ function SelectPersona() {
     }
   };
 
+  useEffect(() => {
+    if (persona && !isTaskComplete('/empathize/select-persona')) {
+      markTaskComplete('/empathize/select-persona');
+    }
+  }, [persona]);
+
   const handlePrevCard = () => {
     if (cardContainerRef.current) {
       cardContainerRef.current.scrollBy({ left: -225, behavior: 'smooth' });
@@ -91,66 +92,63 @@ function SelectPersona() {
     }
   };
 
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
-    <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <ActivityPageLayout
-          header="Who will you play as?"
-          phase="Empathize"
-          phaseColor="text-empathize"
-          text={
-            <>
-              Phishers don't trick everyone the same way!
-              <br />
-              Pick a character and see how they deal with online scams:
-            </>
-          }
-          activity={
-            <div className="flex items-center gap-4 w-[780px]">
-              <div className="h-[300px] flex items-center">
-                <button
-                  onClick={handlePrevCard}
-                  disabled={isAtStart}
-                  className={`p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4 ${
-                    isAtStart ? 'opacity-50 cursor-not-allowed' : ''
-                  }`}
-                >
-                  &lt;
-                </button>
-              </div>
-              <div
-                ref={cardContainerRef}
-                className="flex gap-4 overflow-x-auto scroll-smooth w-full"
-              >
-                {personaCards.map((card) => (
-                  <img
-                    key={card._id}
-                    src={card.cardImageUrl}
-                    alt="persona-card"
-                    className={`w-52 cursor-pointer rounded-lg transition-all duration-300 
-                ${persona?._id === card._id ? 'scale-110 shadow-lg' : ''}`}
-                    onClick={() => handlePersonaSelection(card)}
-                  />
-                ))}
-              </div>
-              <div className="h-[300px] flex items-center">
-                <button
-                  onClick={handleNextCard}
-                  disabled={isAtEnd}
-                  className={`p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4 ${
-                    isAtEnd ? 'opacity-50 cursor-not-allowed' : ''
-                  }`}
-                >
-                  &gt;
-                </button>
-              </div>
-            </div>
-          }
-        />
-      )}
-    </>
+    <ActivityPageLayout
+      header="Who will you play as?"
+      phase="Empathize"
+      phaseColor="text-empathize"
+      text={
+        <>
+          Phishers don't trick everyone the same way! <br />
+          Pick a character and see how they deal with online scams:
+        </>
+      }
+      activity={
+        <div className="flex items-center gap-4 w-[780px]">
+          <div className="h-[300px] flex items-center">
+            <button
+              onClick={handlePrevCard}
+              disabled={isAtStart}
+              className={`p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4 ${
+                isAtStart ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+            >
+              &lt;
+            </button>
+          </div>
+          <div
+            ref={cardContainerRef}
+            className="flex gap-4 overflow-x-auto scroll-smooth w-full"
+          >
+            {personaCards.map((card) => (
+              <img
+                key={card._id}
+                src={card.cardImageUrl}
+                alt="persona-card"
+                className={`w-52 cursor-pointer rounded-lg transition-all duration-300 
+                  ${persona?._id === card._id ? 'scale-110 shadow-lg' : ''}`}
+                onClick={() => handlePersonaSelection(card)}
+              />
+            ))}
+          </div>
+          <div className="h-[300px] flex items-center">
+            <button
+              onClick={handleNextCard}
+              disabled={isAtEnd}
+              className={`p-2 rounded-full text-bold text-define transition duration-200 text-xl bg-empathize px-4 ${
+                isAtEnd ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+            >
+              &gt;
+            </button>
+          </div>
+        </div>
+      }
+    />
   );
 }
 
