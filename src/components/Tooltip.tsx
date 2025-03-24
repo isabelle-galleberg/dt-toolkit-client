f38df384b358353245d5f07aaa235c803e3ed9dc
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-24 12:37:50 +0100

    feat: tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,29 @@
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
+
+interface TooltipProps {
+  text: string;
+  position: 'top' | 'bottom' | 'left' | 'right';
+}
+
+function Tooltip({ text, position }: TooltipProps) {
+  return (
+    <div className="relative group inline-block">
+      <div
+        className={`absolute hidden group-hover:block bg-base-100 text-primary text-xs p-2 rounded-md w-max z-10 transition-all ${
+          position === 'top'
+            ? 'bottom-full mb-2 left-1/2 transform -translate-x-1/2'
+            : position === 'bottom'
+              ? 'top-full mt-2 left-1/2 transform -translate-x-1/2'
+              : position === 'left'
+                ? 'right-full mr-2 top-1/2 transform -translate-y-1/2'
+                : 'left-full ml-2 top-1/2 transform -translate-y-1/2'
+        }`}
+      >
+        {text}
+      </div>
+      <QuestionMarkCircleIcon className="w-6 h-6" />
+    </div>
+  );
+}
+
+export default Tooltip;
