39cb5bcd151433cd0d20bf8d92afa67adfd00ac4
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-26 09:25:56 +0100

    style: add max width

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,14 +9,14 @@ function Tooltip({ text, position }: TooltipProps) {
   return (
     <div className="relative group inline-block">
       <div
-        className={`absolute hidden group-hover:block bg-base-100 text-primary text-xs p-2 rounded-md w-max z-10 transition-all ${
+        className={`absolute hidden group-hover:block bg-[#214A6B] text-primary text-xs p-2 rounded-md w-max z-10 transition-all ${
           position === 'top'
             ? 'bottom-full mb-2 left-1/2 transform -translate-x-1/2'
             : position === 'bottom'
               ? 'top-full mt-2 left-1/2 transform -translate-x-1/2'
               : position === 'left'
                 ? 'right-full mr-2 top-1/2 transform -translate-y-1/2'
-                : 'left-full ml-2 top-1/2 transform -translate-y-1/2'
+                : 'left-full ml-2 top-1/2 transform -translate-y-1/2 max-w-60'
         }`}
       >
         {text}
