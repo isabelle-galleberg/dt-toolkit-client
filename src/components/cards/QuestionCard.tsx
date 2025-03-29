960bb3f9f3ee5bba767ccbde781c48ca0772d09a
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-29 11:25:20 +0100

    feat: question card

---
+++ src/components/cards/QuestionCard.tsx
@@ -0,0 +1,27 @@
+import question from '../../assets/question.svg';
+
+interface CardProps {
+  text: string;
+  classFront?: string;
+  classBack?: string;
+}
+
+function IdeateCard({ text, classFront, classBack }: CardProps) {
+  return (
+    <label className="swap swap-flip cursor-pointer">
+      <input type="checkbox" readOnly />
+      <div
+        className={`hover:scale-105 swap-off card w-48 h-64 shadow-xl p-4 flex justify-center text-center items-center ${classFront}`}
+      >
+        <img src={question} />
+      </div>
+      <div
+        className={`swap-on card w-48 h-64 shadow-xl p-4 flex justify-center text-center items-center ${classBack}`}
+      >
+        <div>{text}</div>
+      </div>
+    </label>
+  );
+}
+
+export default IdeateCard;
